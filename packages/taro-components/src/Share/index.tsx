3dcad747e96f7f4dfbaad234f0f981f69d06acb5
Fairfarren <lykwowlol@gmail.com>
2025-07-14 23:23:56 +0800

    feat: 添加下拉

--- packages/taro-components/src/Share/index.tsx
+++ packages/taro-components/src/Share/index.tsx
@@ -1,70 +1,86 @@
-import type { CommonEventFunction } from '@tarojs/components'
 import { Image, ScrollView, View } from '@tarojs/components'
 import type Taro from '@tarojs/taro'
 import { useShareAppMessage } from '@tarojs/taro'
 import classnames from 'classnames'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import iconLoading from './assets/loading.svg'
 import styles from './index.module.scss'
 
 interface ShareOption {
-	title: string
-	imageUrl: string
-	path: string
+    title: string
+    imageUrl: string
+    path: string
 }
 
 export interface ShareProps {
-	children: JSX.Element | JSX.Element[]
-	promise?: (e?: Taro.ShareAppMessageObject) => Promise<ShareOption>
-	shareOption?: {
-		title?: string
-		imageUrl?: string
-		path?: string
-	}
-	onScrollToLower?: CommonEventFunction<object>
-	scrollLoading?: boolean
-	className?: string
+    children: JSX.Element | JSX.Element[]
+    promise?: (e?: Taro.ShareAppMessageObject) => Promise<ShareOption>
+    shareOption?: {
+        title?: string
+        imageUrl?: string
+        path?: string
+    }
+    onScrollToLower?: () => void
+    scrollLoading?: boolean
+    onRefresherPulling?: () => void
+    className?: string
 }
 /**
  * 分享
  */
 export const Share = (props: ShareProps) => {
-	useShareAppMessage((e) => {
-		if (props.promise && e.from === 'button') return props.promise(e)
+    const [refresherTriggered, setRefresherTriggered] = useState(false)
 
-		return {
-			...props.shareOption,
-		}
-	})
+    useShareAppMessage((e) => {
+        if (props.promise && e.from === 'button') return props.promise(e)
 
-	const children = useMemo(() => props.children, [props])
+        return {
+            ...props.shareOption,
+        }
+    })
 
-	return (
-		<ScrollView
-			className={classnames(
-				'share-scroll',
-				styles.scrollView,
-				props.className,
-			)}
-			scrollY
-			onScrollToLower={props?.onScrollToLower}
-		>
-			{children}
-			<View
-				className={classnames(
-					'share-scroll-container',
-					styles.bottomView,
-					!props.scrollLoading && styles.hideBottom,
-				)}
-			>
-				<Image
-					src={iconLoading}
-					className={classnames(
-						'share-scroll-loading',
-						styles.loadingIcon,
-					)}
-				/>
-			</View>
-		</ScrollView>
-	)
+    const children = useMemo(() => props.children, [props])
+
+    async function onRefresherPulling() {
+        if (refresherTriggered) {
+            return
+        }
+        setRefresherTriggered(true)
+        await props?.onRefresherPulling?.()
+        setRefresherTriggered(false)
+    }
+
+    return (
+        <ScrollView
+            className={classnames(
+                'share-scroll',
+                styles.scrollView,
+                props.className,
+            )}
+            refresherEnabled={!!props?.onRefresherPulling}
+            refresherTriggered={refresherTriggered}
+            scrollY
+            onScrollToLower={props?.onScrollToLower}
+            onRefresherPulling={() => {
+                onRefresherPulling()
+            }}
+        >
+            {children}
+            <View
+                className={classnames(
+                    'share-scroll-container',
+                    styles.bottomView,
+                    !props.scrollLoading && styles.hideBottom,
+                )}
+            >
+                <Image
+                    src={iconLoading}
+                    className={classnames(
+                        'share-scroll-loading',
+                        styles.loadingIcon,
+                    )}
+                />
+            </View>
+        </ScrollView>
+    )
 }
